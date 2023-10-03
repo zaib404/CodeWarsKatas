4174f35b8f11f884914ae582f35e03c4528ba212
Zaib <inam1_16@uni.worc.ac.uk>
2023-10-03 10:29:27 +0000

    katas setup

---
+++ CWKatas/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+string sentence = "Welcome";
+
+List<string> rtnList = sentence.Split(' ').ToList();
+    
+    for (int i = 0; i < rtnList.Count; i++)
+    {
+      if (rtnList[i].Count() >= 5)
+      {
+        string rev = "";
+        
+        for (int j = rtnList[i].Length - 1; j >= 0; j--)
+        {
+          rev += rtnList[i][j];
+        }
+        
+        if (rev.Length >= 5)
+        {
+          rtnList[i] = rev;
+        }
+      }
+    }
+    sentence = string.Join(" ", rtnList);
+
+Console.WriteLine(sentence);
